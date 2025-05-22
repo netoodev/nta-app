dec3f7e0723ca7bd5b10f356f610b5d77ebd6783
netoodev <netoodeveloper@gmail.com>
2025-05-22 08:45:00 -0300

    Estilização da header

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 import Header from './components/Header/header';
 import Sidebar from './components/Sidebar/sidebar';
+import profilePic from './img/profile-pic.jpg';
 
 function App() {
   return (
     <div className='main-container'>
       <Sidebar/>
-      <Header/>
+      <Header name='Patrick Ramos' function='Monitor de chaves' img={profilePic}/>
     </div>
   );
 }
--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -1,17 +1,25 @@
 import './header.css';
 import notifIc from '../../img/notif-ic.svg';
-import searchIc from '../../img/search-ic.svg';
 
-function Header() {
+
+function Header(props) {
   return (
     <div className='header'>
         <div>
-            <input placeholder='Procure por uma sala, tipo ou capacidade'></input>
+            <div className="input">
+              <input placeholder='Procure por uma sala, tipo ou capacidade'></input>
+            </div>
             <div className='notif-btn'>
                 <img src={notifIc} alt=''></img>
             </div>
-            <div></div>
-            <div className='profile-btn'></div>
+            <div className='divider'></div>
+            <div className='profile-btn'>
+              <img src={props.img} alt=''></img>
+              <div className='profile-name'>
+                <h4>{props.name}</h4>
+                <p>{props.function}</p>
+              </div>
+            </div>
         </div>
     </div>
   );
