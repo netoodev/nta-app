b7d68070518c7837f1efb0e76460f1a77675315e
netoodev <netoodeveloper@gmail.com>
2025-05-26 18:50:31 -0300

    Estilização do Card de ação

--- src/components/ActionCard/actionCard.js
+++ src/components/ActionCard/actionCard.js
@@ -5,15 +5,33 @@ import notifIc from '../../img/notif-ic.svg';
 
 
 const ActionCard = (props) => (
-    <div className='action-card'>
-        <div className='action-btn'>
-            <img src={phoneIc} alt=''></img>
+    <div className='action-card-main'>
+        <div className='action-card'>
+            <div className='action-btn'>
+                <img src={phoneIc} alt=''></img>
+            </div>
+            <div className='action-btn'>
+                <img src={notifIc} alt=''></img>
+            </div>
+            <div className='action-key-btn'>
+                <p>Receber chave</p>
+            </div>
         </div>
-        <div className='action-btn'>
-            <img src={notifIc} alt=''></img>
-        </div>
-        <div className='action-key-btn'>
-            <p>Receber chave</p>
+        <div className='room-info-card'>
+            <div>
+                <span>Tipo</span>
+                <p>Laboratório Notebook</p>
+            </div>
+            <div>
+                <div>
+                    <span>Capacidade</span>
+                    <p>25 lugares</p>
+                </div>
+                <div>
+                    <span>Cód. Docente</span>
+                    <p>100256325</p>
+                </div>
+            </div>
         </div>
     </div>
 );
